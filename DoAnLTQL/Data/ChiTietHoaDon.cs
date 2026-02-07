93f8046b29dba61dcbadadff30c245ebce8853b1
DTH235774-NguyenTriThien <thien_dth235774@student.agu.edu.vn>
2026-02-07 13:54:28 +0700

    Hoan thanh giai doan 3

---
+++ DoAnLTQL/Data/ChiTietHoaDon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCaPhe.Data
+{
+    public class ChiTietHoaDon
+    {
+        public int ChiTietID { get; set; }
+
+        public string MaHD { get; set; } = null!;
+        public string MaSP { get; set; } = null!;
+
+        public int SoLuong { get; set; }
+        public decimal DonGia { get; set; }
+        public decimal? ThanhTien { get; set; }
+        public string? GhiChu { get; set; }
+
+        public virtual HoaDon HoaDon { get; set; } = null!;
+        public virtual SanPham SanPham { get; set; } = null!;
+    }
+}
---
+++ DoAnLTQL/Data/HoaDon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace QuanLyQuanCaPhe.Data
+{
+    public class HoaDon
+    {
+        public string MaHD { get; set; } = null!;
+        public DateTime? NgayLap { get; set; }
+
+        public string MaNV { get; set; } = null!;
+        public string? MaKH { get; set; }
+
+        public decimal? TongTien { get; set; }
+        public string? TrangThai { get; set; }
+        public string? GhiChu { get; set; }
+        public decimal? GiamGia { get; set; }
+
+        public int idBan { get; set; }
+        public virtual Ban Ban { get; set; } = null!;
+        public virtual KhachHang? KhachHang { get; set; }
+        public virtual NhanVien NhanVien { get; set; } = null!;
+
+        public virtual ObservableCollectionListSource<ChiTietHoaDon> ChiTietHoaDon { get; } = new();
+        public virtual ObservableCollectionListSource<InventoryMovements> InventoryMovements { get; } = new();
+    }
+}
